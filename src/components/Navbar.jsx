088feb799572211a6802e0d2458c2d84cb63486c
Allwan Kece <AlwanRafa08@gmail.com>
2025-08-18 12:08:33 +0700

    Perbaikan

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar() {
             <h1 className="logo">My Portofolio</h1>
             <ul>
                 <li><Link to ="/">Home</Link></li>
-                <li><Link to ="/">About</Link></li>
-                <li><Link to ="/">Projects</Link></li>
-                <li><Link to ="/">Contact</Link></li>
+                <li><Link to ="/about">About</Link></li>
+                <li><Link to ="/projects">Projects</Link></li>
+                <li><Link to ="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
